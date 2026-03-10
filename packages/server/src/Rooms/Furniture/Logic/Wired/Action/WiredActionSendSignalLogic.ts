9c80eb59d50aea6bb08db1d1182fab354149748f
Muff1n <253524507+muff1n-pixel@users.noreply.github.com>
2026-03-10 21:15:12 +0100

    fix: restrict wired signal triggers

--- packages/server/src/Rooms/Furniture/Logic/Wired/Action/WiredActionSendSignalLogic.ts
+++ packages/server/src/Rooms/Furniture/Logic/Wired/Action/WiredActionSendSignalLogic.ts
@@ -8,6 +8,12 @@ export default class WiredActionSendSignalLogic extends WiredLogic {
     }
 
     public async handleTrigger(options?: WiredTriggerOptions): Promise<void> {
+        if(performance.now() - this.lastTriggered < 500) {
+            return;
+        }
+
+        this.setActive();
+
         for(const logic of this.roomFurniture.room.getFurnitureWithCategory(WiredTriggerReceiveSignalLogic)) {
             await logic.handleWiredSignal(this.roomFurniture, options);
         }
