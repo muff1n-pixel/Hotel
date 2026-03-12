88c4bb46bb4dd1607f0eafa83f01dc7e95b876ea
Muff1n <253524507+muff1n-pixel@users.noreply.github.com>
2026-03-12 17:47:01 +0100

    chore: fix pets sorting

--- packages/game/src/UserInterface/components/Shop/Pages/ShopPetsPage.tsx
+++ packages/game/src/UserInterface/components/Shop/Pages/ShopPetsPage.tsx
@@ -41,7 +41,7 @@ export default function ShopPetsPage({ editMode, page }: ShopPageProps) {
     useEffect(() => {
         const filteredPets: ShopPetData[] = [];
 
-        for(const pet of pets.toSorted((petA, petB) => (petB.pet?.breed?.index ?? 0) - (petA.pet?.breed?.index ?? 0))) {
+        for(const pet of pets.toSorted((petA, petB) => (petA.pet?.breed?.index ?? 0) - (petB.pet?.breed?.index ?? 0))) {
             if(pet.pet?.breed && filteredPets.some((filteredPet) => filteredPet.pet?.breed?.id === pet.pet?.breed?.id)) {
                 continue;
             }
