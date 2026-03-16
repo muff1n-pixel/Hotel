716760115656a25920387d532a2f21002a8bb598
Dani-404 <danielfigueiredo.12@hotmail.fr>
2026-03-16 08:27:18 +0100

    Fixing migration

--- packages/server/src/Database/Migrations/20260316080001-add-permission-role-description-column.ts
+++ packages/server/src/Database/Migrations/20260316080001-add-permission-role-description-column.ts
@@ -4,7 +4,7 @@ import type { Migration } from "sequelize-cli";
 export default {
     up: (queryInterface: QueryInterface): Promise<void> => queryInterface.sequelize.transaction(
         async (transaction) => {
-            await queryInterface.addColumn("permissions", "description", {
+            await queryInterface.addColumn("permission_roles", "description", {
                 type: DataTypes.TEXT,
                 allowNull: true
             }, {
@@ -15,7 +15,7 @@ export default {
 
     down: (queryInterface: QueryInterface): Promise<void> => queryInterface.sequelize.transaction(
         async (transaction) => {
-            await queryInterface.removeColumn("permissions", "description", {
+            await queryInterface.removeColumn("permission_roles", "description", {
                 transaction
             });
         }
