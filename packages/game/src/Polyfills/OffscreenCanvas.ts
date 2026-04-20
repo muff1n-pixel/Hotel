8e6293934eee241511e5384be9163723dfb3f647
Muff1n <253524507+muff1n-pixel@users.noreply.github.com>
2026-04-20 21:30:20 +0200

    chore: only polyfill offscreen canvas if undefined

--- packages/game/src/Polyfills/OffscreenCanvas.ts
+++ packages/game/src/Polyfills/OffscreenCanvas.ts
@@ -38,6 +38,6 @@ if (typeof window !== 'undefined') {
         }
 
         window.OffscreenCanvas =
-            OffscreenCanvasPolyfill as unknown as typeof OffscreenCanvas;
+            window.OffscreenCanvas || OffscreenCanvasPolyfill as unknown as typeof OffscreenCanvas;
     //}
 }
