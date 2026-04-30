df819151e3b704327ea44634e439537e8cb80cb5
Muff1n <253524507+muff1n-pixel@users.noreply.github.com>
2026-04-30 21:24:54 +0200

    feat: fix call stacks wired action

--- packages/server/src/Rooms/Furniture/Logic/Wired/Action/WiredActionCallStacksLogic.ts
+++ packages/server/src/Rooms/Furniture/Logic/Wired/Action/WiredActionCallStacksLogic.ts
@@ -9,13 +9,13 @@ export default class WiredActionCallStacksLogic extends WiredActionLogic {
     }
 
     public async handleAction(options?: WiredTriggerOptions): Promise<void> {
-        if(!this.roomFurniture.model.data?.wiredActionToggleRandomState) {
+        if(!this.roomFurniture.model.data?.wiredActionCallStacks) {
             return;
         }
 
         let executedStacks: RoomPositionOffsetData[] = [];
 
-        for(const furnitureId of this.roomFurniture.model.data.wiredActionToggleRandomState.furnitureIds) {
+        for(const furnitureId of this.roomFurniture.model.data.wiredActionCallStacks.furnitureIds) {
             const roomFurniture = this.roomFurniture.room.furnitures.find((roomFurniture) => roomFurniture.model.id === furnitureId);
 
             if(!roomFurniture) {
@@ -23,6 +23,10 @@ export default class WiredActionCallStacksLogic extends WiredActionLogic {
             }
 
             if(executedStacks.some((executedStack) => executedStack.row === roomFurniture.model.position.row && executedStack.column === roomFurniture.model.position.column)) {
+                continue;
+            }
+
+            if(this.roomFurniture.model.position.row === roomFurniture.model.position.row && this.roomFurniture.model.position.column === roomFurniture.model.position.column) {
                 continue;
             }
 
