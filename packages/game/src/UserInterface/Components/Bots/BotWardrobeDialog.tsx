500386a61a5587103ac12ccb14e96fce375e95f9
muff1n-pixel <253524507+muff1n-pixel@users.noreply.github.com>
2026-05-07 19:05:59 +0200

    chore: use FigureConfigurationData.create instead of structuredClone

--- packages/game/src/UserInterface/Components/Bots/BotWardrobeDialog.tsx
+++ packages/game/src/UserInterface/Components/Bots/BotWardrobeDialog.tsx
@@ -2,7 +2,7 @@ import FigureWardrobeDialog from "../Wardrobe/FigureWardrobeDialog";
 import { useCallback, useEffect, useState } from "react";
 import { webSocketClient } from "../../..";
 import { useDialogs } from "../../Hooks/useDialogs";
-import { UpdateRoomBotData, UserBotData } from "@pixel63/events";
+import { FigureConfigurationData, RoomBotsData, UpdateRoomBotData, UserBotData } from "@pixel63/events";
 import WardrobeAvatar from "@UserInterface/Components/Wardrobe/WardrobeAvatar";
 import DialogButton from "@UserInterface/Common/Dialog/Components/Button/DialogButton";
 
@@ -15,10 +15,25 @@ export type BotWardrobeDialogProps = {
 export default function BotWardrobeDialog(props: BotWardrobeDialogProps) {
     const dialogs = useDialogs();
 
-    const [figureConfiguration, setFigureConfiguration] = useState(() => structuredClone(props.data.figureConfiguration));
+    const [figureConfiguration, setFigureConfiguration] = useState(FigureConfigurationData.create(props.data.figureConfiguration));
 
     useEffect(() => {
-      setFigureConfiguration(structuredClone(props.data.figureConfiguration));
+        const listener = webSocketClient.addProtobuffListener(RoomBotsData, {
+            async handle(payload: RoomBotsData) {
+                if(payload.botsRemoved?.some((removedBot) => removedBot.id === props.data.id)) {
+                    dialogs.closeDialog("bot-wardrobe");
+                }
+            },
+        });
+
+        return () => {
+            webSocketClient.removeProtobuffListener(RoomBotsData, listener);
+        };
+    }, [props.data.id, dialogs]);
+
+
+    useEffect(() => {
+        setFigureConfiguration(FigureConfigurationData.create(props.data.figureConfiguration));
     }, [props.data.figureConfiguration]);
 
     const handleApply = useCallback(() => {
