c50e63c836bdfb974656c368523a37a982803b87
Muff1n <253524507+muff1n-pixel@users.noreply.github.com>
2026-07-27 17:28:29 +0200

    fix: use nearest neighbour for landscape texture

--- packages/game/src/Client/Room/Items/Furniture/RoomFurnitureMaskSprite.ts
+++ packages/game/src/Client/Room/Items/Furniture/RoomFurnitureMaskSprite.ts
@@ -47,6 +47,7 @@ export default class RoomFurnitureMaskSprite extends RoomSprite {
     public updateLandscape() {
         if(this.item.roomRenderer.landscape.image) {
             this.sprite.texture = Texture.from(this.item.roomRenderer.landscape.image);
+            this.sprite.texture.source.scaleMode = "nearest";
         }
     }
 
